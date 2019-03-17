9b14060869470fa2f798cd8480f465dc5ec72d07
Matthew Philps <philps.matthew@gmail.com>
2019-03-17 18:31:44 +0900

    wip

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
+const PageOne = () => {
+    return (
+        <div>
+            <Link to='pagetwo'>Navigate To Page Two</Link>
+        </div>
+    )
+}
+
+const PageTwo = () => {
+    return (
+        <div>
+            <button>Click Me</button>
+            <Link to='pageone'>Navigate to Page One</Link>
+        </div>
+    )
+}
 
 const App = () => {
     return <div>App</div>
